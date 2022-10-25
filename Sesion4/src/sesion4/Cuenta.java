befaa0fe1acb70eaf969eb26410e716a98f3b1a7
Gabo-Dev <usuario@pc22-042>
2022-10-25 10:54:09 +0200

    Add all class

---
+++ Sesion4/src/sesion4/Cuenta.java
@@ -0,0 +1,16 @@
+package sesion4;
+
+import java.util.List;
+
+public class Cuenta {
+    private String numero;
+    private String titular;
+    private Double saldo;
+    private List<Movimiento> mMovimientos;
+    public  void ingreso(double dinero){
+        
+    }
+    public  void reintegro(double cantidad){
+        
+    }
+}
